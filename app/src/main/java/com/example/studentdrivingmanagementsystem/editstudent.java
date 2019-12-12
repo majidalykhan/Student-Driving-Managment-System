9ceef284aacc9bb29d5863647bd7f4179c6c38de
Shayan Naveed <54434490+Shayannaveed1998@users.noreply.github.com>
2019-12-12 18:55:17 +0500

    Student and edit student done navigation
    done navigation additional in java class add objects of xml

--- app/src/main/java/com/example/studentdrivingmanagementsystem/editstudent.java
+++ app/src/main/java/com/example/studentdrivingmanagementsystem/editstudent.java
@@ -2,13 +2,37 @@ package com.example.studentdrivingmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class editstudent extends AppCompatActivity {
 
+    ImageButton booklessonbtn;
+    EditText editname,editnextlesson,editmobile,editlicenseno,edittotalprice,edithourrate,editdiscount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editstudent);
+
+        editname=findViewById(R.id.editname);
+        editdiscount=findViewById(R.id.editdiscount);
+        editnextlesson=findViewById(R.id.editnextlesson);
+        editmobile=findViewById(R.id.editmobile);
+        editlicenseno=findViewById(R.id.editlicenseno);
+        edittotalprice=findViewById(R.id.edittotalprice);
+        edithourrate=findViewById(R.id.edithourrate);
+        booklessonbtn = findViewById(R.id.booklessonbtn);
+
+        booklessonbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(editstudent.this, student.class);
+                startActivity(a);
+            }
+        });
+
     }
 }
