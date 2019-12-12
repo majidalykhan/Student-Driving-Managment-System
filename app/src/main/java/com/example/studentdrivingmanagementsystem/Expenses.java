6ec16ccf8bb6eb631a43b593271dcb85e6a278cc
Shayan Naveed <54434490+Shayannaveed1998@users.noreply.github.com>
2019-12-12 19:08:11 +0500

    Expenses and New Expenses Navigation
    Expenses and New Expenses Navigation done with backend java

--- app/src/main/java/com/example/studentdrivingmanagementsystem/Expenses.java
+++ app/src/main/java/com/example/studentdrivingmanagementsystem/Expenses.java
@@ -2,13 +2,43 @@ package com.example.studentdrivingmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Expenses extends AppCompatActivity {
 
+    ImageButton newexpensefuelbtn,newexpensevehiclebtn,newexpenseotherbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+
+        newexpensefuelbtn=findViewById(R.id.newexpensefuelbtn);
+        newexpensevehiclebtn=findViewById(R.id.newexpensevehiclebtn);
+        newexpenseotherbtn=findViewById(R.id.newexpenseotherbtn);
+
+        newexpensefuelbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Expenses.this, NewExpenses.class);
+                startActivity(a);
+            }
+        });
+        newexpensevehiclebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Expenses.this, NewExpenses.class);
+                startActivity(a);
+            }
+        });
+        newexpenseotherbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Expenses.this, NewExpenses.class);
+                startActivity(a);
+            }
+        });
     }
 }
