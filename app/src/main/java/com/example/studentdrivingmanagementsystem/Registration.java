eeb6c5eb08c39672e869827c183d84c83ad467ae
majidalykhan <majidalykhan@gmail.com>
2019-12-15 16:48:07 +0500

    Save Data (Test Successful)

--- app/src/main/java/com/example/studentdrivingmanagementsystem/Registration.java
+++ app/src/main/java/com/example/studentdrivingmanagementsystem/Registration.java
@@ -1,11 +1,21 @@
 package com.example.studentdrivingmanagementsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registration extends AppCompatActivity {
 
@@ -13,6 +23,16 @@ public class Registration extends AppCompatActivity {
              drivingLicenseNo, medicalCondition, reference, note;
     CheckBox provisionalDrivingLicenseChkbox, theoryTestPassedChkbox, previousDrivingChkbox, eyesightChkbox;
     ImageButton uploadBtn , date, submitRegBtn;
+
+
+    private static final String KEY_STUDENTNO = "student_no";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_ADDRESS = "address";
+    private static final String KEY_POSTALCODE = "postalCode";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_MOBILENO = "mobileNo";
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -41,5 +61,42 @@ public class Registration extends AppCompatActivity {
         submitRegBtn = findViewById(R.id.submitRegBtn);
 
 
+        submitRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
+
     }
+
+
+    private void register(){
+
+        String studentNum = studentNo.getText().toString();
+        String studentName = name.getText().toString();
+
+        Map<String, Object> student = new HashMap<>();
+        student.put(KEY_STUDENTNO, studentNum);
+        student.put(KEY_NAME, studentName);
+
+        db.collection("test").document("test student").set(student)
+        .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(Registration.this,"Register Successful",Toast.LENGTH_LONG).show();
+
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Registration.this,"Register Failed",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
 }
