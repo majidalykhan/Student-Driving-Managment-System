fb1db27fd081738269dba289b3cb78c89bf9aee7
Shayan Naveed <54434490+Shayannaveed1998@users.noreply.github.com>
2019-12-21 17:52:43 +0500

    Revert "Spinner Timer added in calander for booking"
    This reverts commit 0d06163fbdd6846f9488068c38257bab4a4107cb.

--- app/src/main/java/com/example/studentdrivingmanagementsystem/calendar.java
+++ app/src/main/java/com/example/studentdrivingmanagementsystem/calendar.java
@@ -4,36 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TimePicker;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class calendar extends AppCompatActivity {
 
-   // EditText calendartimeSelect;
+    EditText calendartimeSelect;
     ImageButton confirmAppointBtn;
-    TimePicker timeSelect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        timeSelect = findViewById(R.id.timeSelect);
+        calendartimeSelect = findViewById(R.id.calendartimeSelect);
         confirmAppointBtn = findViewById(R.id.confirmAppointBtn);
 
-        timeSelect = (TimePicker) findViewById(R.id.timeSelect);
-        timeSelect.setIs24HourView(false); // used to display AM/PM mode
-        // perform set on time changed listener event
-        timeSelect.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-                // display a toast with changed values of time picker
-                Toast.makeText(getApplicationContext(), hourOfDay + "  " + minute, Toast.LENGTH_SHORT).show();
 
-            }
-        });
         confirmAppointBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
