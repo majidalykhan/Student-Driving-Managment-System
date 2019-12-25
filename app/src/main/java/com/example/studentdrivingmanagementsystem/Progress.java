cdf9651fa7fcf5d8fa1b7162a7fc2a72d82ad2f8
Shayan Naveed <54434490+Shayannaveed1998@users.noreply.github.com>
2019-12-26 02:29:19 +0500

    star raiting java done

--- app/src/main/java/com/example/studentdrivingmanagementsystem/Progress.java
+++ app/src/main/java/com/example/studentdrivingmanagementsystem/Progress.java
@@ -3,12 +3,18 @@ package com.example.studentdrivingmanagementsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Progress extends AppCompatActivity {
 
     Spinner spinner;
+    RatingBar ratingbar;
+    Button button;
 
 
     @Override
@@ -25,6 +31,26 @@ public class Progress extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+        setContentView(R.layout.activity_progress);
+        addListenerOnButtonClick();
+    }
+
+    public void addListenerOnButtonClick() {
+        ratingbar = (RatingBar) findViewById(R.id.ratingBar);
+        button = (Button) findViewById(R.id.button);
+        //Performing action on Button Click
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                //Getting the rating and displaying it on the toast
+                String rating = String.valueOf(ratingbar.getRating());
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_LONG).show();
+            }
+
+        });
 
     }
 }
